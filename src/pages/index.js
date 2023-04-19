772fcf69f7287bdbb53ea71c63f45d705990e7f1
CChoi08 <C_Choi7895@yahoo.com>
2023-04-18 17:13:36 -0700

    resume update

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
                 Explore my latest projects showcasing my wide range of programming knowledge & web development skills.
               </p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
-                <Link href="/Christopher.Choi.Resume.November.2022.pdf" target={"_blank"}
+                <Link href="/ChrisResume.pdf" target={"_blank"}
                 className='flex items-center bg-dark text-light p-2.5 px-6
                 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
                 border-2 border-solid border-transparent hover:border-dark
